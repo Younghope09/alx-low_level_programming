6292e3e4a3f30e18b7c33d7b62e29c846bc76ef7
Younghope09 <agboedehope4@gmail.com>
2022-12-09 01:44:58 -0800

    hmmmmmmm

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
  * main -Entry
  * Return: Always 0
  */
+
 int main(void)
 {
 	printf("Size of a char: %1 byte(s)\n", sizeof(char));
@@ -11,5 +13,5 @@ int main(void)
 	printf("Size of a long long int: %1 byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %1 byte(s)\n", sizeof(float));
 
-		return (0);
+	return (0);
 }
