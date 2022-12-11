fea44cf941d19045b618f7fa78a97e969ed3e300
Younghope09 <agboedehope4@gmail.com>
2022-12-11 14:59:49 -0800

    getting it now

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a pieces of art, \n");
+	printf("with a proper grammar, but the outcome is a piece of art, \n");
 	return (0);
 }
