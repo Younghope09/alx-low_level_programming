dbd0780ccc06dd09f487521ed578daceeb842390
Younghope09 <agboedehope4@gmail.com>
2022-12-11 15:10:18 -0800

    getting it now

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
-
 /**
- *
- *  * main - Prints sized of characters
- *
- *   *
- *
- *    *Return: Always 0 (success)
- *
- *     */
-
+ *main - Prints sized of characters
+ *Return: Always 0 (success)
+ */
 int main(void)
-
 {
-
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
@@ -25,5 +16,4 @@ int main(void)
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
-
 }
