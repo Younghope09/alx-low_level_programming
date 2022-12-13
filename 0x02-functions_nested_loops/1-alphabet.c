9c644d11774f7932bb6186e8ac16a8a50d651a59
Younghope09 <agboedehope4@gmail.com>
2022-12-13 13:36:31 -0800

    C programming is fun

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - Entry point
+ * Description: a funtion that prints the alphabet,
+ * in lowercase followed by a new line
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
