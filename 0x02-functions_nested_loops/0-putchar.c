00c9f8b5a956e2dafeabbcbd313950d54c5f5a6d
Younghope09 <agboedehope4@gmail.com>
2022-12-13 13:25:53 -0800

    C programming is fun

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.>
+
+/**
+ * _putchar.c - write the character c to stdout
+ * @c: the character to print
+ *
+ * Return: 0 success 1.
+ * on error, -1 is returned, nd error is sent appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, $c, 1));
+}
