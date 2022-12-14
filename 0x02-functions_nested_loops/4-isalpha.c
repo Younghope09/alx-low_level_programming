43f1955e6ba76956f03d7a7f9e472ad95c0d62e3
Younghope09 <agboedehope4@gmail.com>
2022-12-14 14:32:32 -0800

    coding is sweet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - this is a program to print out alphabets
+ * @c: characters
+ * Return: 1 if c is a letter, lowercase or uppercase, or 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (( c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'z'))
+		return (1);
+	else
+		return (0);
+}
