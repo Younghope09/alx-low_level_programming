82afa1b8aa5f41ab9ac91c499072c021bd7ef08d
Younghope09 <agboedehope4@gmail.com>
2022-12-14 15:59:12 -0800

    On God

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minutes of the day of
+ * jack Bauer, starting from 00:00 to 12:59.
+ */
+void jack_bauer(void)
+
+{
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+
+			_putchar((hour % 10) + '0');
+
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+
+			_putchar((minute % 10) + '0');
+
+			_putchar('\n');
+		}
+	}
+}
