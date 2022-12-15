678b0bead55a7e65660324d548c6fd0815b35ab9
Younghope09 <agboedehope4@gmail.com>
2022-12-14 16:37:23 -0800

    Thank God am here now

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,16 +7,16 @@
 
 void print_to_98(int n)
 {
-	if ( m >= 98)
+	if (n >= 98)
 	{
 		while (n > 98)
-			printf("%d, ", n--);
-		printf("%d\n", n);
+			_printf("%d, ", n--);
+		_printf("%d\n", n);
 	}
 	else
 	{
 		while (n < 98)
-			printf("%d, ", n++);
-		printf("%d\n", n);
+			_printf("%d, ", n++);
+		_printf("%d\n", n);
 	}
 }
