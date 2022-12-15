81133e3dc5763250b4b0e1504b1fba52c901e665
Younghope09 <agboedehope4@gmail.com>
2022-12-15 12:35:08 -0800

    growing

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,19 +1,11 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * main - tests function that prints if integer is positive or negative
- *
- *   * Return: 0
- *
- *    */
-
-
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 
 int main(void)
-
 {
 
 	int i;
@@ -24,8 +16,5 @@ int main(void)
 
 	positive_or_negative(i);
 
-
-
 	return (0);
-
 }
