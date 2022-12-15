6618d47da2016108d1bc8f937b4678d0d4a9ff64
Younghope09 <agboedehope4@gmail.com>
2022-12-14 16:21:45 -0800

    On God

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,23 +23,24 @@ void times_table(void)
 				if (b != 0)
 				{
 					_putchar(' ');
-
 					_putchar((c % 10) + '0');
 				}
 				if (b < 9)
 				{
 					_putchar(',');
-
 					_putchar(' ');
 				}
-				else
+			}
+			else
+			{
+				_putchar((c / 10) + '0');
+			
+				_putchar((c % 10) + '0');
 				{
-					_putchar((c / 10) + '0');
-
-					_putchar((c % 10) +'0');
 					if (b < 9)
 					{
 						_putchar(',');
+
 
 						_putchar(' ');
 					}
