30fbc395589fec5c34f92ebaaf9470e1337b845a
Younghope09 <agboedehope4@gmail.com>
2022-12-16 19:22:37 -0800

    debugging is fun

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,9 +11,7 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-
 	i = 0;
-
 
 	/* while (i < 10)*/
 	/* {*/
@@ -23,5 +21,4 @@ int main(void)
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
-
 }
