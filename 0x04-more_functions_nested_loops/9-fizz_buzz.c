a2bc19f5b5294243e894b46b863d9b1dcbb084da
Younghope09 <agboedehope4@gmail.com>
2022-12-16 17:58:24 -0800

    more nexted loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,7 +3,7 @@
 
 /**
  * main - entry point*
-   Description: prints the numbers 1 - 100 w/
+ * Description: prints the numbers 1 - 100 w/
  * Fizz for multiples of 3, Buzz for multiples of 5,
  * and FizzBuzz for multiples of both
  * Return: always 0
